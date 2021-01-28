73e205a3cdc0666e7322b3dfa8f157fa9b022e14
Ramanan Ramasamy <42319924+ramananr49@users.noreply.github.com>
2021-01-28 18:12:48 +0530

    Update statistics.py
    implemented alert method

--- statistics.py
+++ statistics.py
@@ -8,3 +8,14 @@ def calculateStats(numbers):
     avg_number = sum(numbers)/len(numbers)
     
   return {"avg": avg_number, "min": Min_number, "Max" : Max_number}
+
+class StatsAlerter:
+  def_init_(self, max_threshold, stats_alerters):
+    self.max_threshold = max_threshold
+    self.stats_alerters = stats_alerters
+    
+  def checkAndAlert(self, values):
+    for value in values
+      if value >= self.max_threshold:
+        for alerter in self.stats_alerters:
+          alerter.alert()
