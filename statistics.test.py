ce2a2bd2e00d0d5183bf5b3e21a93d7b96e88f2c
Ramanan Ramasamy <42319924+ramananr49@users.noreply.github.com>
2021-01-28 19:24:35 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -9,7 +9,7 @@ class StatsTest(unittest.TestCase):
     computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
     epsilon = 0.001
     self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
-    self.assertAlmostEqual(computedStats["Max"], 8.9, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
 
   def test_avg_is_nan_for_empty_input(self):
@@ -20,7 +20,7 @@ class StatsTest(unittest.TestCase):
     # Use nan and isnan in https://docs.python.org/3/library/math.html
     self.assertTrue(math.isnan(computedStats["avg"]))
     self.assertTrue(math.isnan(computedStats["min"]))
-    self.assertTrue(math.isnan(computedStats["Max"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
 
   def test_raise_alerts_when_max_above_threshold(self):
     emailAlert = EmailAlert()
