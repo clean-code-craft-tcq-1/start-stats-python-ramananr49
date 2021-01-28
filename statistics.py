89de4563a6f6d6d8674c40dc80d495f6c242ffe3
Ramanan Ramasamy <42319924+ramananr49@users.noreply.github.com>
2021-01-28 20:22:01 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,38 +1,37 @@
-
 def calculateStats(numbers):
-  Max_number = Min_number = avg_number = float('nan')
-  
-  if len(numbers) != 0:
-    Max_number = max(numbers)
-    Min_number = min(numbers)
-    avg_number = sum(numbers)/len(numbers)
-    
-  return {"avg": avg_number, "min": Min_number, "max" : Max_number}
+    maximum_number = minimum_number = avg_number = float('nan')
+
+    if len(numbers) != 0:
+        maximum_number = max(numbers)
+        minimum_number = min(numbers)
+        avg_number = sum(numbers) / len(numbers)
+
+    return {"avg": avg_number, "min": minimum_number, "max": maximum_number}
+
 
 class EmailAlert:
-  def _init_ (self):
-    self.emailSent = False
-    
-  def alert(self):
-    self.emailSent = True
-    
-    
-class LEDAlert:
-  def _init_ (self):
-    self.ledGlows = False
-    
-  def alert(self):
-    self.ledGlows = Ture
-    
-class StatsAlerter:
-  def _init_(self, max_threshold, stats_alerters):
-    self.max_threshold = max_threshold
-    self.stats_alerters = stats_alerters
-    
-  def checkAndAlert(self, values):
-    for value in values:
-      if value >= self.max_threshold:
-        for alerter in self.stats_alerters:
-          alerter.alert()
-          
+    def __init__(self):
+        self.emailSent = False
 
+    def alert(self):
+        self.emailSent = True
+
+
+class LEDAlert:
+    def __init__(self):
+        self.ledGlows = False
+
+    def alert(self):
+        self.ledGlows = True
+
+
+class StatsAlerter:
+    def __init__(self, max_threshold, stats_alerters):
+        self.max_threshold = max_threshold
+        self.stats_alerters = stats_alerters
+
+    def checkAndAlert(self, values):
+        for value in values:
+            if value >= self.max_threshold:
+                for alerter in self.stats_alerters:
+                    alerter.alert()
