aa456ed8418cff7c41add373fd3a7b408ab375cd
Ramanan Ramasamy <42319924+ramananr49@users.noreply.github.com>
2021-01-28 18:29:08 +0530

    Update statistics.py
    dummy method to email & LED alert

--- statistics.py
+++ statistics.py
@@ -2,7 +2,7 @@
 def calculateStats(numbers):
   Max_number = Min_number = avg_number = float('nan')
   
-  if len(numbers) != 0;
+  if len(numbers) != 0:
     Max_number = max(numbers)
     Min_number = min(numbers)
     avg_number = sum(numbers)/len(numbers)
@@ -10,12 +10,27 @@ def calculateStats(numbers):
   return {"avg": avg_number, "min": Min_number, "Max" : Max_number}
 
 class StatsAlerter:
-  def_init_(self, max_threshold, stats_alerters):
+  def _init_(self, max_threshold, stats_alerters):
     self.max_threshold = max_threshold
     self.stats_alerters = stats_alerters
     
   def checkAndAlert(self, values):
-    for value in values
+    for value in values:
       if value >= self.max_threshold:
         for alerter in self.stats_alerters:
           alerter.alert()
+          
+ class EmailAlert:
+  def _init_ (self):
+    self.emailSent = False
+    
+   def alert(self):
+    self.emailSent = True
+    
+    
+ class LEDAlert:
+  def _init_ (self):
+    self.ledGlows = False
+    
+  def alert(self):
+    self.ledGlows = Ture
