ac375bea1f922cec41f90f8f100bb2b479aae125
Ramanan Ramasamy <42319924+ramananr49@users.noreply.github.com>
2021-01-28 18:03:39 +0530

    Update statistics.py
    initial code

--- statistics.py
+++ statistics.py
@@ -1,3 +1,10 @@
 
 def calculateStats(numbers):
-  return None
+  Max_number = Min_number = avg_number = float('nan')
+  
+  if len(numbers) != 0;
+    Max_number = max(numbers)
+    Min_number = min(numbers)
+    avg_number = sum(numbers)/len(numbers)
+    
+  return {"avg": avg_number, "min": Min_number, "Max" : Max_number}
